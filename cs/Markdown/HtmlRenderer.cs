22c4b3acb82047785837fc4624f47a698792330a
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-15 12:29:41 +0500

    Рефакторинг HtmlRenderer. Добавление методов и констант

--- cs/Markdown/HtmlRenderer.cs
+++ cs/Markdown/HtmlRenderer.cs
@@ -5,6 +5,15 @@ using System.Text;
 
 public class HtmlRenderer
 {
+    private const string EmOpen = "<em>";
+    private const string EmClose = "</em>";
+    private const string StrongOpen = "<strong>";
+    private const string StrongClose = "</strong>";
+    private const string HeadingOpen = "<h1>";
+    private const string HeadingClose = "</h1>";
+    private const string ParagraphOpen = "<p>";
+    private const string ParagraphClose = "</p>";
+
     public string Render(IReadOnlyList<Block> blocks)
     {
         var sb = new StringBuilder();
@@ -17,11 +26,11 @@ public class HtmlRenderer
             switch (block.Type)
             {
                 case BlockType.Heading:
-                    sb.Append("<h1>").Append(inner).Append("</h1>");
+                    sb.Append(HeadingOpen).Append(inner).Append(HeadingClose);
                     break;
 
                 case BlockType.Paragraph:
-                    sb.Append("<p>").Append(inner).Append("</p>");
+                    sb.Append(ParagraphOpen).Append(inner).Append(ParagraphClose);
                     break;
             }
 
@@ -32,65 +41,82 @@ public class HtmlRenderer
         return sb.ToString();
     }
 
-    private static string RenderInlines(IReadOnlyList<Node> inlines, BlockType context)
+    private static string RenderInlines(IReadOnlyList<Node> inlines, BlockType blockType)
     {
         var sb = new StringBuilder();
 
         for (int i = 0; i < inlines.Count; i++)
         {
             var node = inlines[i];
+
             switch (node.Type)
             {
                 case NodeType.Text:
-                    sb.Append(node.Text ?? string.Empty);
+                    if (!string.IsNullOrEmpty(node.Text)) 
+                        sb.Append(node.Text);
                     break;
 
                 case NodeType.Em:
-                    sb.Append("<em>")
-                      .Append(node.Text ?? string.Empty)
-                      .Append("</em>");
+                    RenderEm(sb, node);
                     break;
 
                 case NodeType.Strong:
-                    if (context == BlockType.Heading)
-                    {
-                        sb.Append("<strong>")
-                          .Append(node.Text ?? string.Empty);
-
-                        var j = i + 1;
-                        while (j < inlines.Count)
-                        {
-                            var n = inlines[j];
-                            if (n.Type == NodeType.Em)
-                            {
-                                sb.Append("<em>")
-                                  .Append(n.Text ?? string.Empty)
-                                  .Append("</em>");
-                                j++;
-                                continue;
-                            }
-                            if (n.Type == NodeType.Strong)
-                            {
-                                sb.Append(n.Text ?? string.Empty);
-                                j++;
-                                continue;
-                            }
-                            break;
-                        }
-
-                        sb.Append("</strong>");
-                        i = j - 1;
-                    }
+                    if (blockType == BlockType.Heading)
+                        RenderHeadingStrong(sb, inlines, ref i);
                     else
-                    {
-                        sb.Append("<strong>")
-                          .Append(node.Text ?? string.Empty)
-                          .Append("</strong>");
-                    }
+                        RenderStrong(sb, node);
                     break;
             }
         }
 
         return sb.ToString();
+    }
+
+    private static void RenderEm(StringBuilder sb, Node node)
+    {
+        if (!string.IsNullOrEmpty(node.Text))
+            sb.Append(EmOpen).Append(node.Text).Append(EmClose);
+    }
+
+    private static void RenderStrong(StringBuilder sb, Node node)
+    {
+        if (!string.IsNullOrEmpty(node.Text))
+            sb.Append(StrongOpen).Append(node.Text).Append(StrongClose);
+    }
+
+    private static void RenderHeadingStrong(StringBuilder sb, IReadOnlyList<Node> inlines, ref int currentIndex)
+    {
+        sb.Append(StrongOpen);
+
+        var currentNode = inlines[currentIndex];
+        if (!string.IsNullOrEmpty(currentNode.Text))
+            sb.Append(currentNode.Text);
+
+        var nextIndex = currentIndex + 1;
+        while (nextIndex < inlines.Count)
+        {
+            var nextNode = inlines[nextIndex];
+
+            if (nextNode.Type == NodeType.Em)
+            {
+                if (!string.IsNullOrEmpty(nextNode.Text))
+                    sb.Append(EmOpen).Append(nextNode.Text).Append(EmClose);
+                
+                nextIndex++;
+                continue;
+            }
+
+            if (nextNode.Type == NodeType.Strong)
+            {
+                if (!string.IsNullOrEmpty(nextNode.Text))
+                    sb.Append(nextNode.Text);
+                
+                nextIndex++;
+                continue;
+            }
+        }
+
+        sb.Append(StrongClose);
+        currentIndex = nextIndex - 1;
     }
 }
