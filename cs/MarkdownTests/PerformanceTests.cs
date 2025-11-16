d19907ce19684025e8c57bcac9e52b8820ece31f
Roman Gleizer <romy4.gleyzer@mail.ru>
2025-11-16 19:22:23 +0500

    Улучшил тест производительности и добавил тест по вложенности

--- cs/MarkdownTests/PerformanceTests.cs
+++ cs/MarkdownTests/PerformanceTests.cs
@@ -9,42 +9,68 @@ namespace MarkdownTests;
 [TestFixture]
 public class PerformanceTests
 {
-    private const string Paragraph =
-        "# Заголовок __с _разными_ символами__\n\n" +
-        "Текст, _окруженный_ и __сильный__ и \\_экранированный\\_ 12_3 " +
-        "слово ра_зных сл_овах не работает. __Непарные_ и _непарные__ . " +
-        "Внутри __двойного _одинарное_ тоже__ работает. " +
-        "Но _внутри __одинарного__ не_ работает. Конец\\\\\n\n";
+    private static readonly string[] Patterns =
+    [
+        "# Заголовок __с _разными_ символами__\n\n",
+        "Текст, _окруженный_ и __сильный__ и \\_экранированный\\_ 12_3 ",
+        "слово ра_зных сл_овах не работает. __Непарные_ и _непарные__ . ",
+        "Внутри __двойного _одинарное_ тоже__ работает. ",
+        "Но _внутри __одинарного__ не_ работает. Конец\\\\\n\n"
+    ];
 
     private const double AllowedGrowth = 2.0;
+    private const int NestingLevels = 1000;
 
-    private static string BuildInput(int targetChars)
+    private Md _md = null!;
+
+    [SetUp]
+    public void SetUp()
     {
-        var sb = new StringBuilder(targetChars + 1024);
-        while (sb.Length < targetChars)
-            sb.Append(Paragraph);
+        _md = new Md(new BlockSegmenter(), new InlineParser(), new HtmlRenderer());
+    }
+
+    private static string BuildText(int size)
+    {
+        var sb = new StringBuilder(size);
+        var patternIndex = 0;
+
+        while (sb.Length < size)
+        {
+            var pattern = Patterns[patternIndex % Patterns.Length];
+            sb.Append(pattern);
+            patternIndex++;
+        }
+
         return sb.ToString();
     }
 
     [Test]
-    public void Render_InputSizeIncreases_ScalesLinearlyOrBetter()
+    public void Render_TextSizeIncreases_ExecutionTimeLinearlyOrBetter()
     {
-        var md = new Md(new BlockSegmenter(), new InlineParser(), new HtmlRenderer());
-
         var sizes = new[] { 2_000, 16_000, 128_000, 1_000_000 };
         var timesMs = new double[sizes.Length];
 
+        const int iterationsPerSize = 5;
+
         for (int i = 0; i < sizes.Length; i++)
         {
-            var input = BuildInput(sizes[i]);
+            var size = sizes[i];
+            var totalMs = 0.0;
 
-            var sw = Stopwatch.StartNew();
-            var html = md.Render(input);
-            sw.Stop();
+            for (int _ = 0; _ < iterationsPerSize; _++)
+            {
+                var text = BuildText(size);
 
-            html.Should().NotBeNullOrEmpty();
+                var sw = Stopwatch.StartNew();
+                var html = _md.Render(text);
+                sw.Stop();
 
-            timesMs[i] = sw.Elapsed.TotalMilliseconds;
+                html.Should().NotBeNullOrEmpty();
+
+                totalMs += sw.Elapsed.TotalMilliseconds;
+            }
+
+            timesMs[i] = totalMs / iterationsPerSize;
         }
 
         var costPerChar = new double[sizes.Length];
@@ -53,5 +79,25 @@ public class PerformanceTests
 
         for (int i = 1; i < sizes.Length; i++)
             (costPerChar[i] / costPerChar[i - 1]).Should().BeLessThanOrEqualTo(AllowedGrowth);
+    }
+
+    [Test]
+    public void Render_DeepNestedMarkup_DoesNotThrow()
+    {
+        var builder = new StringBuilder();
+
+        for (int i = 0; i < NestingLevels; i++)
+            builder.Append("__вложенный _текст ");
+
+        builder.Append("конец");
+
+        for (int i = 0; i < NestingLevels; i++)
+            builder.Append("_ конец__");
+
+        var input = builder.ToString();
+
+        Action act = () => _md.Render(input);
+
+        act.Should().NotThrow();
     }
 }
